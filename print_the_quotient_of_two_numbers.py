0bdc8cab28546e01dcb3b8a11565abbba8ac0900
Chloe <lunamarychloeanne@gmail.com>
2025-03-12 23:45:54 +0800

    pseudocode adjusment

--- print_the_quotient_of_two_numbers.py
+++ print_the_quotient_of_two_numbers.py
@@ -2,6 +2,6 @@
 num1 = int(input("Enter the 1st number: "))
 num2 = int(input("Enter the 2nd number: "))
 
-# print the quotient of two numbers inserted
+# print the quotient of two numbers inserted with decimal point
 quotient = num1 / num2
 print(f"The quotient of two numbers is {quotient:.2f}")
