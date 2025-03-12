a65bad82b12b232c2ee9697de423da515df8b09f
Chloe <lunamarychloeanne@gmail.com>
2025-03-13 00:45:10 +0800

    some changes and print outcome

--- print_how_many_odd_numbers_in_10_numbers.py
+++ print_how_many_odd_numbers_in_10_numbers.py
@@ -1,5 +1,11 @@
 # input 10 numbers
+odd_count = 0 
+
 for i in range(10):
-    num = int(input(f"Enter number {i + 1}"))
-    
+    num = int(input(f"Enter number {i + 1}: "))
+    if num % 2 != 0:
+        odd_count += 1
+
 # print how many odd numbers inserted by the user
+print(f"The number of odd numbers are {odd_count}")
+
