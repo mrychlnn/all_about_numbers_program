5e8dbc779c7d7c089d8fe3c666a998bc113f314b
Chloe <lunamarychloeanne@gmail.com>
2025-03-13 01:57:06 +0800

    print output

--- print_the_difference_of_two_numbers.py
+++ print_the_difference_of_two_numbers.py
@@ -3,3 +3,5 @@ num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
 
 # print the difference of two numbers inserted by the user
+difference = num1 - num2
+print(f"The difference between two numbers is {difference:.2f}")
