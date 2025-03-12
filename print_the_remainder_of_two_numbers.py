8d37f7dac0c8b6b3f95d0e1d1237a3327847aef3
Chloe <lunamarychloeanne@gmail.com>
2025-03-13 02:25:07 +0800

    print output

--- print_the_remainder_of_two_numbers.py
+++ print_the_remainder_of_two_numbers.py
@@ -3,3 +3,5 @@ num1 = int(input("Enter the 1st number: "))
 num2 = int(input("Enter the 2nd number: "))
 
 # print the remainder when 1st number is divided to 2nd number
+remainder = num1 % num2 
+print(f"The remainder when {num1} divided by {num2} is {remainder}")
