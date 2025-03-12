ef7a008ba912b51e780d2cc073357b164da1de9f
Chloe <lunamarychloeanne@gmail.com>
2025-03-12 23:43:50 +0800

    print outcome

--- print_the_quotient_of_two_numbers.py
+++ print_the_quotient_of_two_numbers.py
@@ -3,3 +3,5 @@ num1 = int(input("Enter the 1st number: "))
 num2 = int(input("Enter the 2nd number: "))
 
 # print the quotient of two numbers inserted
+quotient = num1 / num2
+print(f"The quotient of two numbers is {quotient:.2f}")
