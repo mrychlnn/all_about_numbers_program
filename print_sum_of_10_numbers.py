dc56f93ee0199dff40cfdb48ab6e29ddcead6536
Chloe <lunamarychloeanne@gmail.com>
2025-03-13 00:16:38 +0800

    adjustments and print outcome

--- print_sum_of_10_numbers.py
+++ print_sum_of_10_numbers.py
@@ -1,5 +1,9 @@
 # input 10 numbers
-for num in range(10):
-    num = float(input(f"Enter number {num+1}: "))
-    
+total = 0
+
+for i in range(10):
+    num = float(input(f"Enter number {i+1}: "))
+    total += num
+
 # print the sum of all the numbers
+print(f"The sum of all numbers is {total:.2f}")
