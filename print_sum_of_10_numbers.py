af0139074f930d7d26a378532ddd783cf82972dd
Chloe <lunamarychloeanne@gmail.com>
2025-03-13 00:33:47 +0800

    adjustment in spacing

--- print_sum_of_10_numbers.py
+++ print_sum_of_10_numbers.py
@@ -2,7 +2,7 @@
 total = 0
 
 for i in range(10):
-    num = float(input(f"Enter number {i+1}: "))
+    num = float(input(f"Enter number {i + 1}: "))
     total += num
 
 # print the sum of all the numbers
