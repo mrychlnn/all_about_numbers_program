cab053e04eedeb6c4063924dba2e578915c3c5e3
Chloe <lunamarychloeanne@gmail.com>
2025-03-12 22:51:11 +0800

    print outcome

--- print_the_bigger_number_between_2_numbers.py
+++ print_the_bigger_number_between_2_numbers.py
@@ -3,3 +3,7 @@ num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
 
 # print the bigger number
+if num1 > num2:
+    print(f"{num1} is the bigger number")
+else:
+    print(f"{num2} is the bigger number")
