b333ae8464cfd98416c8124c554475b8b1dbb0a2
Chloe <lunamarychloeanne@gmail.com>
2025-03-12 23:09:23 +0800

    print outcome

--- print_equal_if_number_is_the_same.py
+++ print_equal_if_number_is_the_same.py
@@ -3,3 +3,7 @@ num1 = int(input("Enter the 1st number: "))
 num2 = int(input("Enter the 2nd number: "))
 
 # print the word "EQUAL" if two number is the same
+if num1 == num2:
+    print("Both numbers are equal.")
+else:
+    print("The two numbers are not equal.")
