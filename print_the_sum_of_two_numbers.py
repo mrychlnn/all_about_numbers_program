9ca777aab301f39b2fdc86ac7eaca4158fa781c1
Chloe <lunamarychloeanne@gmail.com>
2025-03-12 23:21:03 +0800

    print outcome

--- print_the_sum_of_two_numbers.py
+++ print_the_sum_of_two_numbers.py
@@ -3,3 +3,5 @@ num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
 
 # print the sum of two numbers inserted
+total = num1 + num2
+print(f"The sum of two numbers is {total:.2f}")
