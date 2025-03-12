40c41d1350cc2f8dea6c2dd619fc80bb5b90fa0d
Chloe <lunamarychloeanne@gmail.com>
2025-03-13 01:34:17 +0800

    print outcome

--- print_the_smaller_number_between_2_numbers.py
+++ print_the_smaller_number_between_2_numbers.py
@@ -3,3 +3,7 @@ num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
 
 # print the smaller number inserted by the user
+if num1 > num2:
+    print(f"{num2:.2f} is the smaller number")
+else: 
+    print(f"{num1:.2f} is the smaller number")
