99877d6bc5e5816d313b6f3a8ba9ad97b782e8f4
Chloe <lunamarychloeanne@gmail.com>
2025-03-12 23:59:12 +0800

    print outcome

--- print_result_when_first_number_is_raised_to_second_number.py
+++ print_result_when_first_number_is_raised_to_second_number.py
@@ -3,3 +3,5 @@ num1 = int(input("Enter the 1st number: "))
 num2 = int(input("Enter the 2nd number: "))
 
 # print result when 1st num is raised to 2nd num
+result = num1 ** num2
+print(f"The result when {num1} is raised to the power of {num2} is {result}")
