1f91ae1d3c59da796d1ba666af0ae570723cf335
Chloe <lunamarychloeanne@gmail.com>
2025-03-13 01:46:56 +0800

    print output

--- print_not_equal_if_number_is_not_the_same.py
+++ print_not_equal_if_number_is_not_the_same.py
@@ -3,3 +3,7 @@ num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
 
 # print the word "NOT EQUAL" if two number is not the same
+if num1 != num2:
+    print("The two numbers are not equal.")
+else:
+    print("Both numbers are equal.")
