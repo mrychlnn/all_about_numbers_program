586ce230b4c37a7fd2ee720089f2dccf319e5d6f
Chloe <lunamarychloeanne@gmail.com>
2025-03-12 23:31:41 +0800

    print outcome

--- print_the_product_of_two_numbers.py
+++ print_the_product_of_two_numbers.py
@@ -3,3 +3,5 @@ num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
 
 # print the product of two numbers inserted
+result = num1 * num2 
+print(f"The product of two numbers is {result:.2f}")
