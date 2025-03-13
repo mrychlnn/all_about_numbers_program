1291f63743a17f85ae67ff126117c995b091389c
Chloe <lunamarychloeanne@gmail.com>
2025-03-13 21:27:07 +0800

    input 10 num

--- print_all_numbers_that_don't_have_duplicate.py
+++ print_all_numbers_that_don't_have_duplicate.py
@@ -1,3 +1,9 @@
 # Initialize numbers for list
+numbers = []
+
 # input 10 numbers
+for i in range(10):
+    num = int(input(f"Enter number {i + 1}: "))
+    numbers.append(num)
+    
 # print all the unique numbers
