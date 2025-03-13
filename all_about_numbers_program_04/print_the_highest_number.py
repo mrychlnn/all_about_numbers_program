11277139e8eabdb0d08d7fe69bcb3e229a513eab
Chloe <lunamarychloeanne@gmail.com>
2025-03-14 00:53:10 +0800

    print output

--- all_about_numbers_program_04/print_the_highest_number.py
+++ all_about_numbers_program_04/print_the_highest_number.py
@@ -1,3 +1,6 @@
+# initialize a list
+numbers = []
+
 # input numbers until the users input is invalid (anything non-numeric)
 while True:
     user_input = input("Enter a number (anything non-numeric to stop): ")
@@ -5,5 +8,10 @@ while True:
     if not user_input.isdigit():
         print("Invalid input.")
         break
-    
+
 # print the highest number entered by the user
+    num = int(user_input)
+    numbers.append(num)
+
+if numbers:
+    print("\nThe highest number is", max(numbers))
