d1ac7b4b41adfc6511cb3316511157c7e2949d67
Chloe <lunamarychloeanne@gmail.com>
2025-03-14 01:20:38 +0800

    print output

--- all_about_numbers_program_04/print_the_average.py
+++ all_about_numbers_program_04/print_the_average.py
@@ -1,3 +1,6 @@
+# initizialize an empty list
+numbers = []
+
 # input numbers until the users input is invalid (anything non-numeric)
 while True:
     user_input = input("Enter a number (anything non-numeric to stop): ")
@@ -7,3 +10,9 @@ while True:
         break
 
 # print the average of entered numbers
+    num = int(user_input)
+    numbers.append(num)
+
+if numbers:
+    average = sum(numbers) / len(numbers)
+    print(f"\nAverage of the entered numbers: {average:.2f}")
