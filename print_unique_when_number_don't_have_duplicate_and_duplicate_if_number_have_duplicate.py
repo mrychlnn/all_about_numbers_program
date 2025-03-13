1018c62f18377092a85c244652f14b3ca903ea75
Chloe <lunamarychloeanne@gmail.com>
2025-03-13 22:13:01 +0800

    pseudocode part 23

---
+++ print_unique_when_number_don't_have_duplicate_and_duplicate_if_number_have_duplicate.py
@@ -0,0 +1,2 @@
+# input numbers until the users input is invalid (non-numeric number)
+# print "Unique" for numbers that don't have duplicate and "Duplicate" for numbers that have duplicate (side of the numbers)
