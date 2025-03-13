d4c208ca07254d473eb725e2b7a036c02426f11c
Chloe <lunamarychloeanne@gmail.com>
2025-03-14 00:41:11 +0800

    print output

--- all_about_numbers_program_04/print_the_number_with_the_most_number_of_duplicate.py
+++ all_about_numbers_program_04/print_the_number_with_the_most_number_of_duplicate.py
@@ -1,3 +1,5 @@
+# initialize a list
+numbers = []
 # input numbers until the users input is invalid (anything non-numeric)
 while True:
     user_input = input("Enter a number (anything non-numeric to stop): ")
@@ -5,5 +7,11 @@ while True:
     if not user_input.isdigit():
         print("Invalid input.")
         break
-    
+
 # print the number with the most number of duplicate entered by the user
+    num = int(user_input)
+    numbers.append(num)
+
+if numbers:
+    most_frequent = max(set(numbers), key=numbers.count)
+    print(f"\nNumber with the most duplicates: {most_frequent}")
