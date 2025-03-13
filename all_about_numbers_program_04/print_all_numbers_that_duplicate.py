84c51ee94531b25f9dbe9193cd1152c8c426e786
Chloe <lunamarychloeanne@gmail.com>
2025-03-14 00:14:12 +0800

    print output

--- all_about_numbers_program_04/print_all_numbers_that_duplicate.py
+++ all_about_numbers_program_04/print_all_numbers_that_duplicate.py
@@ -1,5 +1,18 @@
+# initialize a list
+numbers = []
+
+# initialize a set for duplicate number to store
+duplicates = set()
+
 # input 10 numbers
 for i in range(10):
     num = int(input(f"Enter number {i + 1}: "))
-    
+    numbers.append(num)
+
 # print all duplicate numbers
+for num in numbers:
+    if numbers.count(num) > 1:
+        duplicates.add(num)
+
+if duplicates:
+    print("Duplicated numbers are:", duplicates)
