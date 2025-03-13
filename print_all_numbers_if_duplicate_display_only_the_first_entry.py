54bbba2d02e6457bf3b597e1098e10387b5ea57b
Chloe <lunamarychloeanne@gmail.com>
2025-03-13 21:58:12 +0800

    initialization and input 10 num

--- print_all_numbers_if_duplicate_display_only_the_first_entry.py
+++ print_all_numbers_if_duplicate_display_only_the_first_entry.py
@@ -1,2 +1,12 @@
+# initialize an empty list
+numbers = []
+
+# initialize set keep track for the seen numbers
+seen = set()
+
 # input 10 numbers
+for i in range(10):
+    num = int(input(f"Enter number {i + 1}: "))
+    numbers.append(num)
+
 # print all numbers if duplicate display only the 1st entry
