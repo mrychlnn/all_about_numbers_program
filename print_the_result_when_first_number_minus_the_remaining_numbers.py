272ffcdcb38cc411492ed794f76ae1e5dbfe2949
Chloe <lunamarychloeanne@gmail.com>
2025-03-13 12:34:31 +0800

    input 1st num

--- print_the_result_when_first_number_minus_the_remaining_numbers.py
+++ print_the_result_when_first_number_minus_the_remaining_numbers.py
@@ -1,3 +1,5 @@
 # input 1st number
+num1 = float(input("Enter number 1:"))
+
 # input the remaining numbers (9 numbers)
 # print the result when 1st number minus all of the remaining numbers inserted by the user
