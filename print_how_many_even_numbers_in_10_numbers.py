c5f191d72fa5c7f1a6d192ee2ac70556e474b24a
Chloe <lunamarychloeanne@gmail.com>
2025-03-13 12:58:32 +0800

    print output

--- print_how_many_even_numbers_in_10_numbers.py
+++ print_how_many_even_numbers_in_10_numbers.py
@@ -1,5 +1,11 @@
+# initialize the count of even numbers
+even_count = 0 
+
 # input 10 numbers
 for i in range(10):
     num = int(input(f"Enter number {i + 1}: "))
-    
+    if num % 2 == 0:
+        even_count += 1
+
 # print how many even numbers in 10 numbers inserted by the user
+print(f"The number of even numbers are {even_count}")
