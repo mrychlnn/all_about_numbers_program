b6979371da78cf373882c2725c0d658834ceccf4
Chloe <lunamarychloeanne@gmail.com>
2025-03-14 01:00:20 +0800

    users input

--- all_about_numbers_program_04/print_the_number_from_highest_to_lowest.py
+++ all_about_numbers_program_04/print_the_number_from_highest_to_lowest.py
@@ -1,2 +1,9 @@
 # input numbers until the users input is invalid (anything non-numeric)
+while True:
+    user_input = input("Enter a number (anything non-numeric to stop): ")
+
+    if not user_input.isdigit():
+        print("Invalid input.")
+        break
+    
 # print the numbers from highest to lowest
