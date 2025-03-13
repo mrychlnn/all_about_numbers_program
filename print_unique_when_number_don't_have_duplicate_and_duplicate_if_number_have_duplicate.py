a8790f111b1cf9b7b8f7fc26147ed882a52b4792
Chloe <lunamarychloeanne@gmail.com>
2025-03-13 22:53:57 +0800

    print output

--- print_unique_when_number_don't_have_duplicate_and_duplicate_if_number_have_duplicate.py
+++ print_unique_when_number_don't_have_duplicate_and_duplicate_if_number_have_duplicate.py
@@ -1,3 +1,6 @@
+# initialize set for unique numbers
+numbers = set()  
+
 # input numbers until the users input is invalid (anything non-numeric)
 while True:
     user_input = input("Enter a number (anything non-numeric to stop): ")
@@ -5,5 +8,12 @@ while True:
     if not user_input.isdigit():
         print("Invalid input.")
         break
-    
+
 # print "Unique" for numbers that don't have duplicate and "Duplicate" for numbers that have duplicate (side of the numbers) 
+    num = int(user_input)
+
+    if num in numbers:
+        print("Duplicate")
+    else:
+        print("Unique")
+        numbers.add(num)  
